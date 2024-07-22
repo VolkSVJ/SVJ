4ea72c036c730211888514bee6a80764e33c3314
JP <jpszva@hotmail.com>
2024-07-21 20:12:42 -0500

    About List

--- hello.py
+++ hello.py
@@ -1,3 +1,15 @@
 for i in [5, 4, 3, 2, 1]:
     print(i)
 print('Boom')
+#Change value in position 
+a = [21, 22, 23]
+a [0] = 500
+print(a)
+#Count Characters
+print(len(a))
+#Print 
+t = [9, 1, 3, 22, 54, 54]
+print(t[1:3])
+print(t[:4])
+print(t[4:])
+print(t[:])
